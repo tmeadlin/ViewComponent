215378a8631742d0f43d86fe4483f59a9b388371
Tyler Meadlin <tyler.meadlin@gmail.com>
2016-12-28 21:17:07 -0600

    Updates to show better ViewComponent potential

---
+++ src/BlogViewComponent/ViewComponents/QuickExample1ViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogViewComponent.ViewComponents
+{
+    public class QuickExample1ViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/BlogViewComponent/ViewComponents/QuickExample3ViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogViewComponent.ViewComponents
+{
+    [ViewComponent(Name="ExampleUsingAttributeName")]
+    public class QuickExample3ViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
